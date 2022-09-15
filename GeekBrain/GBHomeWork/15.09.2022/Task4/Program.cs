6ff1c57109fddc74120a47b44f3d05486f295d77
Daniil Filatov <danikfil@ya.ru>
2022-09-15 19:25:50 +0300

    AddTask4

--- GeekBrain/GBHomeWork/15.09.2022/Task4/Program.cs
+++ GeekBrain/GBHomeWork/15.09.2022/Task4/Program.cs
@@ -4,3 +4,23 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
+Console.WriteLine("Введите первое число, ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число, ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число, ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (max < number1 )
+{
+ max = number1 ;
+}
+if (max < number2)
+{
+ max = number2;
+}
+if (max < number3)
+{
+ max = number3;
+}
+Console.WriteLine($"Максимальное из этих чисел,{max} ");
