12e589c06de6f411fa2f69f1aad78201c1278d46
Daniil Filatov <danikfil@ya.ru>
2022-09-19 23:11:32 +0300

    addHomeWorkTask15

--- GeekBrain/GBHomeWork/19.09.2022/Task015/Program.cs
+++ GeekBrain/GBHomeWork/19.09.2022/Task015/Program.cs
@@ -3,3 +3,17 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+bool WeekEnd(int wknd)
+{
+    return wknd ==6 || wknd ==7;
+}
+
+Console.WriteLine("Введите число соответствующее дню недели, ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number <1  || number > 7) Console.WriteLine("Нет такого дня недели");
+else 
+{
+ if  (WeekEnd(number))Console.Write("Да"); 
+ else Console.Write("Нет");
+}
