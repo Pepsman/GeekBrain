462fc1a3e5028555d62c27112b1ac943a4fb4be9
Daniil Filatov <danikfil@ya.ru>
2022-09-19 19:12:46 +0300

    addSeminar

---
+++ GeekBrain/17.09.2022/Task002/Program.cs
@@ -0,0 +1,22 @@
+﻿// 14. Напишите программу, которая принимает на
+// вход число и проверяет, кратно ли оно
+// одновременно 7 и 23.
+// 14 -> нет
+// 46 -> нет
+// 161 -> да
+
+bool Multiplicity (int num)
+{
+  {
+    return num % 7 == 0 && num % 23 == 0;
+}
+
+}
+
+
+
+Console.WriteLine("Введите первое число, ");
+bool number = Multiplicity(Console.ReadLine());
+//if (number % 7 == 0 && number % 23 == 0) Console.WriteLine($"{number}, кратно ");
+else Console.WriteLine($"{number},  не кратно ");
+
---
+++ GeekBrain/Warcode/001/Program.cs
@@ -0,0 +1,9 @@
+﻿public class CustomMath 
+
+{
+    public static int multiply=(int a, int b);
+    {
+        return = ($"{a}, {b}");
+    }
+}
+
