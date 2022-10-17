280aa1a82d106aaf8af10ce09c88546a4bdb62f1
Daniil Filatov <danikfil@ya.ru>
2022-10-17 08:34:10 +0300

    BugFixHomeWork12.10.2022

--- GeekBrain/GBHomeWork/12.10.2022/Task68/Program.cs
+++ GeekBrain/GBHomeWork/12.10.2022/Task68/Program.cs
@@ -17,7 +17,7 @@ int AkkermanFunction(int m, int n)
     if (m > 0 && n == 0)
         return AkkermanFunction(m - 1, 1);
     else
-        return AkkermanFunction(m - 1, Akkerman(m, n - 1));
+        return AkkermanFunction(m - 1, AkkermanFunction(m, n - 1));
 }
 
 
