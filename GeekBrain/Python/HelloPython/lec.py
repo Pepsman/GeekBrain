36e453608f2552b16e7a734fdb87601c1032efe4
Daniil Filatov <danikfil@ya.ru>
2022-11-23 17:34:46 +0300

    addHomeWork

---
+++ GeekBrain/Python/HelloPython/lec.py
@@ -0,0 +1,11 @@
+a = 123
+b = 1.23
+print (a)
+print (b)
+value = None
+value = 1234
+print (type(value))
+s = 'hello world'
+print (f'{a} - {b} - {s}')
+#f = True
+#print (f)
---
+++ GeekBrain/Python/Seminar1/Task3.py
@@ -0,0 +1,6 @@
+#Вывести на экран числа от -N до N
+
+n = int(input("Введите число: "))
+#for i in range(-n,n+1):
+#    print(i, end =' ')
+print (*range(-n,n+1))
